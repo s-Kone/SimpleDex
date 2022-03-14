bfcd9ddd55b7398a384ede0f8f67b627df43132a
waterman <76515579+a-waterson@users.noreply.github.com>
2022-03-13 18:30:45 -0700

    fixed init_db

--- modules/database_init.js
+++ modules/database_init.js
@@ -1,18 +1,18 @@
 var pool = require('./pool.js');
 
 // TODO: write a sql script that will initialize database and tables, checking if each exists on the way
-let sql = 'create table if not exists';
+// let sql = 'create table if not exists';
 
 const init_db = () => {
-    pool.query(sql, function(err, results){
+    // pool.query(sql, function(err, results){
         
-        if (err) {
-            console.log(err)
-        }
-        else {
-            console.log(results);
-        }
-    });
+    //     if (err) {
+    //         console.log(err)
+    //     }
+    //     else {
+    //         console.log(results);
+    //     }
+    // });
 };
 
 exports.init_db = init_db;
