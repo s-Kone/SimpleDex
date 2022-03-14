07452fabaf49c82a84a44b5fbe0dfc29ac61eee4
Alex G <giasson.alexj@gmail.com>
2022-03-13 19:19:04 -0700

    insert admin user

--- server/sql/static_data.sql
+++ server/sql/static_data.sql
@@ -8,3 +8,8 @@ INSERT INTO UserType (UserTypeID, UserTypeDesc)
 VALUES
     (1, 'Admin'),
     (2, 'User');
+
+-- TODO: store passwords as hash
+INSERT INTO User (Username, UserPassword, UserTypeID, DateCreatedUTC)
+VALUES
+    ('admin', '1234abcd', 1, UTC_TIMESTAMP);
