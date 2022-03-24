0cd2cacba3d1699dcc4f75c333e1f47a635ef477
Khurkhartsaga Munkhbold <60725072+Saga910@users.noreply.github.com>
2022-03-23 18:11:23 -0700

    Added User Login and register.

---
+++ client/js/Login.js
@@ -0,0 +1,23 @@
+const xhttp = new XMLHttpRequest();
+const endPointRoot = "https://alexgiasson.me/comp4537/termproject/api/v1/admin/";
+
+function getADLogin() {
+    let u = document.getElementById("user").value;
+    let p = document.getElementById("pass").value;
+    let params = '?username=' + u + '&password=' + p;
+    let url = endPointRoot + params;
+    xhttp.open("GET", url, true);
+    xhttp.send(url);
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            document.getElementById('adminStats').innerHTML = this.responseText;
+        }
+    }
+}
+
+function onRegister() {
+}
+
+function onClick() {
+    
+}
