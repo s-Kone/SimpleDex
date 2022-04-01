c58c6a7ef82e8cc221978c8f77516c8ac715f6c5
Alex G <giasson.alexj@gmail.com>
2022-04-01 16:57:56 -0700

    new team/pokemon schema

--- server/sql/createTeamRecord.sql
+++ server/sql/createTeamRecord.sql
@@ -1,8 +1,13 @@
 CREATE TABLE TeamRecord (
-        TeamRecordID    INT NOT NULL AUTO_INCREMENT,
-        TeamID          INT NOT NULL,
-        UserID          INT NOT NULL,
-        PokemonID       INT NOT NULL,
+        TeamRecordID     INT NOT NULL AUTO_INCREMENT,
+        UserTeamID       INT NOT NULL,
+        UserID           INT NOT NULL,
+        PokemonID1       INT NOT NULL,
+        PokemonID2       INT NOT NULL,
+        PokemonID3       INT NOT NULL,
+        PokemonID4       INT NOT NULL,
+        PokemonID5       INT NOT NULL,
+        PokemonID6       INT NOT NULL,
         DateModifiedUTC DATETIME,
         PRIMARY KEY (TeamRecordID),
         FOREIGN KEY (UserID) REFERENCES User(UserID)
