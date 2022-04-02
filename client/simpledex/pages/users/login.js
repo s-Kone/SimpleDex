52d7be7951379e84c5a1bc8f5bb2474aca75b745
Alex G <giasson.alexj@gmail.com>
2022-04-02 14:56:46 -0700

    forms recreated. no post functionality

---
+++ client/simpledex/pages/users/login.js
@@ -0,0 +1,102 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+export default function Login() {
+    const loginUser = event => {
+        event.preventDefault() // don't redirect the page
+
+        // TODO: post to SimpleDex API /login endpoint
+    }
+
+    return ( 
+        <>
+            <Head>
+                <title>SimpleDex Login</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1>Login</h1>
+                    <form onSubmit={loginUser}>
+                    <label htmlFor="Email">Email</label>
+                    <input type="text" placeholder="Email" id="email" required />
+
+                    <label htmlFor="password">Password</label>
+                    <input type="password" placeholder="Password" id="password" required />
+
+                    <button type="submit">Login</button>
+
+                </form>
+
+                <Link href="/">
+                    <a>Back to Home</a>
+                </Link>
+            </main>
+
+            <style jsx>{`
+                form *{
+                    font-family: 'Poppins',sans-serif;
+                    outline: none;
+                    border: none;
+                    letter-spacing: 0.5px;
+                }
+                
+                form h3{
+                    font-size: 32px;
+                    font-weight: 500;
+                    line-height: 30px;
+                    text-align: center;
+                }
+                
+                form {
+                    height: 450px;
+                    width: 400px;
+                    font-weight: 500;
+                    background-color: rgba(169, 231, 255, 0.13);
+                    position: absolute;
+                    transform: translate(-50%,-50%);
+                    top: 50%;
+                    left: 50%;
+                    border-radius: 10px;
+                    backdrop-filter: blur(10px);
+                    border: 2px solid rgba(255,255,255,0.1);
+                    box-shadow: 0 0 40px rgba(8,7,16,0.6);
+                    padding: 50px 35px;
+                }
+                
+                button {
+                    margin-top: 25px;
+                    width: 100%;
+                    background-color: #FFC1A8;
+                    color: #080710;
+                    padding: 15px 0;
+                    font-size: 18px;
+                    font-weight: 500;
+                    border-radius: 5px;
+                    cursor: pointer;
+                }
+                
+                input{
+                    display: block;
+                    height: 50px;
+                    width: 100%;
+                    background-color: #D4F3FF;
+                    border-radius: 3px;
+                    padding: 0 10px;
+                    margin-top: 8px;
+                    font-size: 14px;
+                    font-weight: 300;
+                }
+                
+                label{
+                    display: block;
+                    margin-top: 20px;
+                    font-size: 16px;
+                    font-weight: 500;
+                }
+            `}</style>
+        </>
+
+
+    )
+}
