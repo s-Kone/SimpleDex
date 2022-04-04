9db54540cf3d644ecf2d83e9a2b9ef8e11ed7659
Khurkhartsaga Munkhbold <60725072+Saga910@users.noreply.github.com>
2022-04-03 18:12:29 -0700

    added pokemon ability and progress bar

--- client/simpledex/components/search/pokemon_display.jsx
+++ client/simpledex/components/search/pokemon_display.jsx
@@ -5,6 +5,7 @@ import { pokemon_lookups } from '../../lookups/pokemon_lookup'
 import { AutoComplete } from "../util/autocomplete";
 import Stats from "../pokemon/stats";
 import Types from "../pokemon/types";
+import Abilities from "../pokemon/ability"
 
 export function PokemonDisplay() {
     const [pokemon, setPokemon] = useState(null)
@@ -21,11 +22,16 @@ export function PokemonDisplay() {
     return (
         <div >
             <AutoComplete data={pokemon_lookups} name={setName} />
-            <button onClick={handleClick}>delete</button>
+            <div className={styles.box}>
+                <button onClick={handleClick} className={styles.button}>Search</button>
+            </div>
             {loading ? <div className={styles.container}>
                 <Sprite pokemon={pokemon} />
                 <p>{pokemon.name}</p>
+                <h3><u>Types</u></h3>
                 <Types pokemon={pokemon} />
+                <h3><u>Abilities</u></h3>
+                <Abilities pokemon={pokemon} />
                 <Stats pokemon={pokemon} />
             </div> : <div></div>}
         </div>
