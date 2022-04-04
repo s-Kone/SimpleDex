41648e7754d571cdc35ae8d3bd651b7acf3dc7d5
Aidan <aidanwaterson@gmail.com>
2022-04-03 22:00:26 -0700

    working on stuff

--- client/simpledex/components/teambuilder/pokemonCard.jsx
+++ client/simpledex/components/teambuilder/pokemonCard.jsx
@@ -7,6 +7,31 @@ import styles from './teambuilder.module.css'
 export const PokemonCard = ({ pokemon_name, jsondata }) => {
     const [data, setData] = useState(null)
     const [loaded, setLoaded] = useState(false)
+    const [formState, updateForm] = useState({
+        name: "",
+        gender: "",
+        level: "",
+        item: "",
+        ability: "",
+        move1: "",
+        move2: "",
+        move3: "",
+        move4: "",
+        types: [],
+        sprite: "",
+        stats: []
+    })
+    const handleChange = e => {
+        const { name, value } = e.target
+        updateForm(prevState => ({
+            ...prevState, [name]: value
+        }))
+    }
+
+    const handleSubmit = () => {
+
+        console.log(formState)
+    }
     useEffect(() => {
         const getPokemonData = async () => {
             const request = `https://alexgiasson.me/comp4537/termproject/api/v1/searchPokemon/name?name=${pokemon_name}`
@@ -18,38 +43,42 @@ export const PokemonCard = ({ pokemon_name, jsondata }) => {
             }).then((response) => {
                 setData(response.data)
                 setLoaded(true)
+            }).catch((e) => {
+                console.log(e)
             })
         }
         getPokemonData()
     }, [])
     return (
+
         <div>
+            <button onClick={handleSubmit}></button>
             {data &&
                 <>
                     <h1>{data.name}</h1>
                     <div>
                         <h3>level</h3>
-                        <input type="text" name="level" />
+                        <input type="text" name="level" onChange={handleChange} value={formState.level} />
                     </div>
                     <div>
                         <h3>gender</h3>
-                        <input type="text" name="gender" />
+                        <input type="text" name="gender" onChange={handleChange} value={formState.gender} />
                     </div>
                     <div>
                         <h3>Item</h3>
-                        <input type="text" name="Item" />
+                        <input type="text" name="item" onChange={handleChange} value={formState.item} />
                     </div>
                     <div>
                         <h3>Ability</h3>
-                        <input type="text" name="Ability" />
+                        <input type="text" name="ability" onChange={handleChange} value={formState.ability} />
                     </div>
                     <h2>Moves</h2>
                     <div className={styles.moves}>
                         <ul>
-                            <li> <input classname={styles.move} type="text" name="move1" /></li>
-                            <li> <input classname={styles.move} type="text" name="move2" /></li>
-                            <li> <input classname={styles.move} type="text" name="move3" /></li>
-                            <li> <input classname={styles.move} type="text" name="move4" /></li>
+                            <li> <input className={styles.move} type="text" name="move1" onChange={handleChange} value={formState.move1} /></li>
+                            <li> <input className={styles.move} type="text" name="move2" onChange={handleChange} value={formState.move2} /></li>
+                            <li> <input className={styles.move} type="text" name="move3" onChange={handleChange} value={formState.move3} /></li>
+                            <li> <input className={styles.move} type="text" name="move4" onChange={handleChange} value={formState.move4} /></li>
                         </ul>
                     </div>
                     <Types pokemon={data} />
